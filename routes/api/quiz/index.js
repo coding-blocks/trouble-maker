8bef726863bdb0a1b2c63b7b78aabac9a620bcae
Abhishek Gupta <hi@itsabhishek.me>
2018-08-08 13:40:37 +0530

    make /quizzes/* adminOnly

--- routes/api/quiz/index.js
+++ routes/api/quiz/index.js
@@ -3,8 +3,11 @@ const routes = require('express').Router()
 const DB = require('../../../models')
 
 const passport = require('../../../passport/index')
+const { adminOnly } = require('../../../passport/middlewares')
 
 const controller = new BaseController(DB.quizzes)
+
+routes.use(passport.authenticate('bearer', {session: false}), adminOnly)
 
 routes.get('/', controller.handleQuery)
 routes.get('/:id', controller.handleQueryById)
@@ -13,7 +16,7 @@ routes.patch('/:id', controller.handleUpdateById)
 routes.delete('/:id', controller.handleDeleteById)
 
 // TODO: write the handleSubmit function in controller.
-routes.post('/:id/submit', passport.authenticate('bearer', {session: false}), (req, res, next) => {
+routes.post('/:id/submit', (req, res, next) => {
   if (req.query.showAnswers) {
     // make sure req.user is admin
     if (req.user.role === 'ADMIN') {
