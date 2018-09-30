e9a6fc72c606604f4ee7c3d1088a9b538b7dd505
Abhishek Gupta <hi@itsabhishek.me>
2018-09-30 17:24:21 +0530

    change type question.description to TEXT

--- models/questions.js
+++ models/questions.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: true
     },
     difficulty: {
